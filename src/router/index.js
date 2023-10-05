2f09f5cf92642fa16b0abf6ce3e951d2b081d24b
Xecades <i@xecades.xyz>
2023-10-05 12:01:04 +0800

    lazyload is not a good idea

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const cap = (x) => x.charAt(0).toUpperCase() + x.slice(1);
-const $ = (x) => () => import(`../views/${cap(x)}View.vue`);
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import SponsorView from "@/views/SponsorView.vue";
+import SponsorshipView from "@/views/SponsorshipView.vue";
+import LabView from "@/views/LabView.vue";
+import FriendView from "@/views/FriendView.vue";
+import TimelineView from "@/views/TimelineView.vue";
+import ErrorView from "@/views/ErrorView.vue";
 
 const routes = [
-    { path: "/", name: "home", component: $("home") },
-    { path: "/about", name: "about", component: $("about") },
-    { path: "/lab", name: "lab", component: $("lab") },
-    { path: "/friend", name: "friend", component: $("friend") },
-    { path: "/sponsor", name: "sponsor", component: $("sponsor") },
-    { path: "/sponsorship", name: "sponsorship", component: $("sponsorship") },
-    { path: "/timeline", name: "timeline", component: $("timeline") },
-    { path: "/:pathMatch(.*)", name: "error", component: $("error") },
+    { path: "/", name: "HomeView", component: HomeView },
+    { path: "/about", name: "AboutView", component: AboutView },
+    { path: "/sponsor", name: "SponsorView", component: SponsorView },
+    { path: "/sponsorship", name: "SponsorshipView", component: SponsorshipView },
+    { path: "/lab", name: "LabView", component: LabView },
+    { path: "/friend", name: "FriendView", component: FriendView },
+    { path: "/timeline", name: "TimelineView", component: TimelineView },
+    { path: "/:pathMatch(.*)", name: "ErrorView", component: ErrorView },
 ];
 
 const router = createRouter({
