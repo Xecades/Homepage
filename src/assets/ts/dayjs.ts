1e622763372c71de4c41ecfa80a6dc11901c79a5
Xecades <i@xecades.xyz>
2025-01-15 20:39:48 +0800

    fix: remove unnecessary dayjs.ts

--- src/assets/ts/dayjs.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-import locale_zhcn from "dayjs/locale/zh-cn";
-
-dayjs.extend(relativeTime);
-dayjs.locale(locale_zhcn);
-
-export default dayjs;
